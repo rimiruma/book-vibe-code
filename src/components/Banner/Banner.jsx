ccb92592c9bf8e48d43e7ca31bfc4a6a097fa1df
rimiruma <rimiruma12@gmail.com>
2025-10-06 04:25:31 +0600

    added a banner done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import BookImg from '../../assets/books.jpg'
 
 const Banner = () => {
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                    src={BookImg}
                     className="max-w-sm rounded-lg shadow-2xl"
                 />
                 <div>
-                    <h1 className="text-5xl font-bold">Books to freshen up your bookshelf</h1>
+                    <h1 className="text-5xl font-bold">Books to freshen up <br /> your bookshelf</h1>
                     <p className="py-6">
                         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                         quasi. In deleniti eaque aut repudiandae et a id nisi.
                     </p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <button className="btn bg-[#23BE0A] rounded-lg text-white p-7">View The List</button>
                 </div>
             </div>
         </div>
---
+++ src/components/Pages/Books/Books.jsx
@@ -0,0 +1,10 @@
+
+const Books = () => {
+    return (
+        <div>
+          <h1>Hello i am books</h1>  
+        </div>
+    );
+};
+
+export default Books;
