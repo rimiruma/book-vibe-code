94fcfb0b057372f0460b6bcd23a60fed4770c079
rimiruma <rimiruma12@gmail.com>
2025-10-06 03:56:56 +0600

    added a footer done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,19 @@
 import { createBrowserRouter } from "react-router";
 import Root from "../components/Pages/Root/Root";
 import ErrorPage from "../components/Pages/ErrorPage/ErrorPage";
+import Home from "../components/Pages/Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
-    errorElement: <ErrorPage></ErrorPage>
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        index: true,
+        path: "/",
+        Component: Home,
+      }
+    ]
   },
 ]);
